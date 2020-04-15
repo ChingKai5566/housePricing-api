c0547c9d8ba6cf3f9810ecd4b96ec585263bce4d
ChingKai5566 <chingkai.hung5566@gamil.com>
2020-04-15 19:39:51 +0800

    add collection

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb+srv://Admin-ChingKai56:Uglyno1.@cluster0-qicjs.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Admin-ChingKai56:Uglyno1.@cluster0-qicjs.mongodb.net/test?retryWrites=true&w=majority/houseDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
